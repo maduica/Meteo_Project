9aa019e9ab353cb81ca07d8eb2e4182a73a2f686
Duica Marius <duica.marius.duica@mta.ro>
2021-01-16 11:00:25 +0200

    Update final pentru JsonAnalyzer
    Toate functiile de get sunt implementate acum.
    
    Signed-off-by: Duica Marius <duica.marius.duica@mta.ro>

--- src/main/java/ro/mta/se/lab/model/JsonAnalyzer.java
+++ src/main/java/ro/mta/se/lab/model/JsonAnalyzer.java
@@ -1,10 +1,51 @@
 package ro.mta.se.lab.model;
 
-public class JsonAnalyzer {
-    StringBuffer jsonFile;
 
-    public JsonAnalyzer(StringBuffer j)
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JsonAnalyzer {
+    String jsonFile_;
+
+    public JsonAnalyzer(String j)
     {
-        this.jsonFile=j;
+        this.jsonFile_=j;
+       // System.out.println(this.jsonFile_);
+    }
+
+    public String get_TEMP_(){
+
+        JSONObject obj=new JSONObject(this.jsonFile_);
+        return  String.valueOf(obj.getJSONObject("main").getFloat("temp"));
+    }
+    public String get_min_TEMP_(){
+
+        JSONObject obj=new JSONObject(this.jsonFile_);
+        return  String.valueOf(obj.getJSONObject("main").getFloat("temp_min"));
+    }
+    public String get_max_TEMP_(){
+
+        JSONObject obj=new JSONObject(this.jsonFile_);
+        return  String.valueOf(obj.getJSONObject("main").getFloat("temp_max"));
+    }
+    public String get_Action(){
+
+        JSONObject obj=new JSONObject(this.jsonFile_);
+        JSONArray wea=(JSONArray)obj.get("weather");
+        JSONObject wea1=(JSONObject)wea.get(0);
+        return (String)wea1.get("main");
+        //return  obj.getJSONObject("weather").get(0).ge;
+    }
+
+    public String getWindSpeed_()
+    {
+        JSONObject obj=new JSONObject(this.jsonFile_);
+        return  String.valueOf(obj.getJSONObject("wind").getFloat("speed"));
+    }
+
+    public String pressure()
+    {
+        JSONObject obj=new JSONObject(this.jsonFile_);
+        return  String.valueOf(obj.getJSONObject("main").getFloat("pressure"));
     }
 }
