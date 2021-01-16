8d4fd80d146c9591809d63ceb8a5754935679742
Duica Marius <duica.marius.duica@mta.ro>
2021-01-16 10:39:34 +0200

    Update JsonAnalyzer
    In aces moment clasa JsonAnalyzer este capabila de a parsa
    fisierul json
    
    Signed-off-by: Duica Marius <duica.marius.duica@mta.ro>

--- src/main/java/ro/mta/se/lab/model/Weather.java
+++ src/main/java/ro/mta/se/lab/model/Weather.java
@@ -1,4 +1,51 @@
 package ro.mta.se.lab.model;
 
 public class Weather {
+    String temperature_;
+    String wind_speed;
+    String pressure_;
+    String maxTemp_;
+    String minTemp_;
+    String action_;
+    String feels_like_;
+
+    public Weather(String temperature, String wind_speed, String pressure_, String maxTemp_, String minTemp_, String action_, String feels_like_) {
+        this.temperature_ = temperature;
+        this.wind_speed = wind_speed;
+        this.pressure_ = pressure_;
+        this.maxTemp_ = maxTemp_;
+        this.minTemp_ = minTemp_;
+        this.action_ = action_;
+        this.feels_like_ = feels_like_;
+    }
+
+    public String getTemperature_() {
+        return temperature_;
+    }
+
+    public String getWind_speed() {
+        return wind_speed;
+    }
+
+    public String getPressure_() {
+        return pressure_;
+    }
+
+    public String getMaxTemp_() {
+        return maxTemp_;
+    }
+
+    public String getMinTemp_() {
+        return minTemp_;
+    }
+
+    public String getAction_() {
+        return action_;
+    }
+
+    public String getFeels_like_() {
+        return feels_like_;
+    }
+
+
 }
